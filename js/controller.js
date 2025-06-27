eca06929789875adb869c0cb81fa506c45f91ebf
Jeffisnothere <100958158+jeffersoncgo@users.noreply.github.com>
2025-06-27 13:03:40 -0400

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -78,3 +78,85 @@ class Controller {
 }
 
 if (typeof module !== 'undefined') module.exports = Controller;
+
+// Example of usage
+// const searchItems = async (Name, library, query) => {
+//   if (!query) {
+//     query = {}
+//     if (Name)
+//       query.Name = Name.trim();
+//     if (library)
+//       query.Library = library.trim();
+//   }
+
+//   searchParams = { ...query };
+
+//   if (!areLibrariesLoaded) {
+//     const limit = 20;
+//     for (let count = 0; count < limit; count++) {
+//       await new Promise(resolve => setTimeout(resolve, 500));
+//       if (areLibrariesLoaded)
+//         break;
+//     }
+//   }
+
+//   if (!areLibrariesLoaded) {
+//     events.onSearchFinish([])
+//     return [];
+//   }
+
+//   // Validate library exists
+//   if (searchParams.Library && (!Libraries?.[searchParams.Library]?.Items)) {
+//     events.onSearchFinish([])
+//     return [];
+//   }
+
+//   let items = [];
+//   if (searchParams.Library)
+//     items = Libraries[searchParams.Library].Items
+//   else
+//     for (lName in Libraries)
+//       items = items.concat(Libraries[lName].Items)
+//   items = await searchInArray(items, searchParams.Name)
+
+//   // Sort items
+//   const sortKey = searchParams.sortBy;
+//   if (sortKey === "Random") {
+//     items = items.sort(() => 0.5 - Math.random());
+//   } else {
+//     items = items.sort((a, b) => {
+//       if (searchParams.order === "desc") {
+//         [a, b] = [b, a];
+//       }
+//       const valA = a?.[sortKey];
+//       const valB = b?.[sortKey];
+
+//       if (typeof valA === "string" && typeof valB === "string") {
+//         return valA.localeCompare(valB);
+//       }
+
+//       if (typeof valA === "number" && typeof valB === "number") {
+//         return valA - valB;
+//       }
+
+//       if (valA instanceof Date && valB instanceof Date) {
+//         return valA.getTime() - valB.getTime();
+//       }
+
+//       return 0;
+//     });
+//   }
+
+//   searchParams.hasPreviousPage = hasPreviousPage();
+//   searchParams.hasNextPage = hasNextPage(items);
+
+//   items = items.slice(searchParams.offset, searchParams.offset + searchParams.limit);
+
+//   events.onSearchFinish(items)
+//   return items;
+// }
+// const controller = new Controller(searchItems.bind(this));
+// searchItems = controller;
+// searchItems = searchItems.exec.bind(searchItems);
+// searchItems.Controller = controller;
+// searchItems('cat pictures')
